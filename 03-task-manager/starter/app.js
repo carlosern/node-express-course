85061fdad6d96814fce8f03feae7f2ac1eaa0add
Carlos Rojas <crojas@myxsales.com>
2021-10-18 09:00:56 -0400

    Hello App

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,13 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+const port = 3200;
+
+// routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+app.listen(port, console.log(`server is listening on port ${port}...`));
+
+console.log("Task Manager App");
