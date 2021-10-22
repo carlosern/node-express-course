5a5e23b3ee88574f7224f94eb2084e2db72d87f7
Carlos Rojas <crojas@myxsales.com>
2021-10-22 08:17:40 -0400

    Create Task operativo

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,27 +1,30 @@
-const getAllTasks = (req, res) => {
-  res.send("all items");
-};
+const Task = require('../models/Task')
 
-const createTask = (req, res) => {
-  res.json(req.body);
-};
+const getAllTasks = (req, res) => {
+    res.send('all items')
+}
+
+const createTask = async (req, res) => {
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
+}
 
 const getTask = (req, res) => {
-  res.json({ id: req.params.id });
-};
+    res.json({ id: req.params.id })
+}
 
 const updateTask = (req, res) => {
-  res.send("update task");
-};
+    res.send('update task')
+}
 
 const deleteTask = (req, res) => {
-  res.send("delete task");
-};
+    res.send('delete task')
+}
 
 module.exports = {
-  getAllTasks,
-  createTask,
-  getTask,
-  updateTask,
-  deleteTask,
-};
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask,
+}
